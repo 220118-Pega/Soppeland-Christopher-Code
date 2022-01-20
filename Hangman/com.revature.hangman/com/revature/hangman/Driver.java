01cafca6d403f304bf3ab4d3bfbe59b2c66b4088
christopher-soppeland <christopher.soppeland@revature.net>
2022-01-20 08:13:01 -0600

    Hid mystery word
    display guesses and word progress in dialog box

--- Hangman/com.revature.hangman/com/revature/hangman/Driver.java
+++ Hangman/com.revature.hangman/com/revature/hangman/Driver.java
@@ -20,6 +20,7 @@ public class Driver {
 		String mysteryWord = JOptionPane.showInputDialog("Enter word to be guessed: ");
 		List<String> correctlyGuessedLetters = new ArrayList<String>();
 		String originalWord = mysteryWord;
+		String wordOuput = "";
 		
 		while(bodyParts > 0) {
 			//Looking for letter in word
@@ -31,32 +32,44 @@ public class Driver {
 			String guessedLetter = JOptionPane.showInputDialog("Guess a letter in the word: ");
 			if(mysteryWord.contains(guessedLetter)) {
 				//true run this
-				System.out.println("the word contains " + guessedLetter);
+				//System.out.println("the word contains " + guessedLetter);
+				JOptionPane.showMessageDialog(null, "the word contains " + guessedLetter);
 				//Remove correctly guessed letters
 				mysteryWord = mysteryWord.replaceAll(guessedLetter,  "");
 				correctlyGuessedLetters.add(guessedLetter);
 			} else {
 				//false run this
-				System.out.println("the word does not contain " + guessedLetter);
+				//System.out.println("the word does not contain " + guessedLetter);
+				JOptionPane.showMessageDialog(null, "the word does not contains " + guessedLetter);
+				
 				//decrement the bodyParts
 				bodyParts--;
 			}
-			System.out.println("How the guessing is going: ");
+			//System.out.println("How the guessing is going: ");
 			for(Character letter:originalWord.toCharArray()) {
 				if(correctlyGuessedLetters.contains(letter.toString())) {
-					System.out.print(letter);
+					//System.out.print(letter);
+					wordOuput += letter.toString();
 				} else {
-					System.out.print("#");
+					//System.out.print("#");
+					wordOuput += "#";
+					
 				}
 			}
-			System.out.println();
+			
+			//System.out.println();
 			if(mysteryWord.length() == 0) {
-				System.out.println("Congradulations you've guessed all the letters in the word");
+				//System.out.println("Congratulations you've guessed all the letters in the word");
+				JOptionPane.showMessageDialog(null, "How the guessing is going: \n" + wordOuput + "\nCongratulations you've guessed all the letters in the word");
 				break;
 			}
+			
+			JOptionPane.showMessageDialog(null, "How the guessing is going: \n" + wordOuput);
+			wordOuput = "";
 		}
 		if(bodyParts == 0) {
-			System.out.println("Better luck next time");
+			//System.out.println("Better luck next time");
+			JOptionPane.showMessageDialog(null, "Better luck next time");
 		}
 	}
 
